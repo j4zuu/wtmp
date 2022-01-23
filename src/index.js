dc578cc2a3876de61d43f81fb50c6fccbc65b21e
j4zuu <jasu.honkanen2@gmail.com>
2022-01-23 14:39:29 +0200

    task done

--- src/index.js
+++ src/index.js
@@ -1,70 +1,14 @@
-import LunchMenu from './assets/LunchMenu';
+import {
+  coursesEn,
+  coursesFi,
+  sortBy,
+  sort,
+  sorter,
+  looper,
+} from './modules/SodexoData';
 
-const field = document.querySelector('.foodField');
-const foodSubmit = document.querySelector('.foodSubmit');
-let foodName = '';
+sorter(coursesEn, sortBy);
+sorter(coursesFi, sortBy);
+looper(coursesEn);
 
-const foods = [
-  {name: 'Lingonberry jam', price: 4.00},
-  {name: 'Mushroom and bean casserole', price: 5.50},
-  {name: 'Chili-flavoured wheat', price: 3.00},
-  {name: 'Vegetarian soup', price: 4.80},
-  {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00},
-];
-
-foodSubmit.addEventListener('click', function() {
-  foodName = field.value;
-  const regExp = /^[A-ZÖÄÅ][a-zöäå,.\s()]{3,64}$/;
-  console.log(regExp.test(foodName));
-});
-
-foods.sort((a, b) => {
-  return a.price - b.price;
-});
-
-const filterByPrice = (item) => {
-  return item.price < 5;
-};
-
-const filtered = foods.filter(filterByPrice);
-console.log(filtered);
-
-const multiply = (item) => {
-  return item.price *= 1.15;
-};
-const multiplied = foods.map(multiply);
-console.log(multiplied);
-
-console.log(foods);
-
-const summed = foods.map(food => food.price).reduce((a, b) => a + b);
-console.log(summed);
-
-/**
- * Return true if passed item is vegan
- * @param item
- * @returns {boolean}
- */
-const filteredByVeg = (item) => {
-  return item.Diets.includes('Veg');
-};
-
-const temp = LunchMenu.LunchMenus[0].SetMenus;
-let vegFilteredList = [];
-
-/**
- * takes 2 parameters, input object and output object.
- * Loops through input and only adds those meals to output that get true from
- * the filteredByVeg filtering
- * @param object
- * @param outPut
- */
-const looper = (object, outPut) => {
-  for (let i = 0; i < object.length; i++) {
-    const temp2 = object[i].Meals.filter(filteredByVeg);
-    outPut.push(temp2);
-  }
-};
-
-looper(temp, vegFilteredList);
-console.log(vegFilteredList);
+sort.innerText = `Sort by: ${sortBy}`;
---
+++ src/modules/SodexoData.js
@@ -0,0 +1,93 @@
+import LunchMenu from './JSONfiles/LunchMenu.JSON';
+import LunchMenuEn from './JSONfiles/LunchMenuEn.JSON';
+
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const langSwitch = document.getElementById('langSwitch');
+const sort = document.getElementById('sort');
+const pickRandom = document.getElementById('pickRandom');
+let lang = 'en';
+let sortBy = 'asc';
+
+const coursesEn = [];
+const coursesFi = [];
+
+const looper = (courses) => {
+  box1.innerText = '';
+  for (let i = 0; i < courses.length; i++) {
+    box1.innerText += courses[i] + ' \n';
+  }
+};
+/**
+ * Since task 3's description was so vague I couldn't really
+ * understand what menu/menus were intended to be seen
+ * So I just hardcoded first meals of first day
+ */
+const tempFi = LunchMenu.LunchMenus[0].SetMenus[0].Meals;
+const tempEn = LunchMenuEn.LunchMenus[0].SetMenus[0].Meals;
+
+const courseGetter = (arr, obj) => {
+  for (let i = 0; i <= Object.keys(obj).length; i++) {
+    if (obj[i] !== undefined) {
+      arr.push(obj[i].Name);
+      console.log(obj[i].Name);
+    }
+  }
+};
+
+courseGetter(coursesFi, tempFi);
+courseGetter(coursesEn, tempEn);
+
+const sorter = (courses, sortBy1) => {
+  if (sortBy1 === 'asc') {
+    courses.sort();
+  } else {
+    courses.sort().reverse();
+  }
+  sort.innerText = `Sort by: ${sortBy1}`;
+  return courses;
+};
+
+const sortSwapper = (sortBy1) => {
+  if (sortBy1 === 'asc') {
+    sortBy = 'desc';
+  } else sortBy = 'asc';
+};
+
+langSwitch.addEventListener('click', function() {
+  if (lang === 'en') {
+    lang = 'fi';
+    if (sortBy === 'asc') {
+      looper(sorter(coursesFi, 'asc'));
+    } else looper(sorter(coursesFi, 'desc'));
+  } else if (lang === 'fi') {
+    if (sortBy === 'asc') {
+      looper(sorter(coursesEn, 'asc'));
+    } else looper(sorter(coursesEn, 'desc'));
+    looper(coursesEn);
+    lang = 'en';
+  }
+  sort.innerText = `Sort by: ${sortBy}`;
+});
+
+sort.addEventListener('click', function() {
+  if (lang === 'en') {
+    if (sortBy === 'asc') {
+      looper(sorter(coursesEn, 'desc'));
+    } else looper(sorter(coursesEn, 'asc'));
+  } else if (lang === 'fi') {
+    if (sortBy === 'asc') {
+      looper(sorter(coursesFi, 'desc'));
+    } else looper(sorter(coursesFi, 'asc'));
+  }
+  sortSwapper(sortBy);
+});
+
+pickRandom.addEventListener('click', function() {
+  if (lang === 'en') {
+    box2.innerText = coursesEn[Math.floor(Math.random() * coursesEn.length)];
+  } else box2.innerText = coursesFi[Math.floor(
+    Math.random() * coursesFi.length)];
+});
+
+export {looper, sort, sorter, sortBy, coursesFi, coursesEn};
